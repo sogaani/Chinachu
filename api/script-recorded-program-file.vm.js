8887d1a3f14e999e3ffd6590e9f0ad655fdcff61
sogaani <soga.ani@gmail.com>
2017-06-23 21:57:55 +0900

    録画済みの番組情報を書き換えるAPIを追加

--- api/script-recorded-program-file.vm.js
+++ api/script-recorded-program-file.vm.js
@@ -1,17 +1,23 @@
 (function() {
-	
+
 	var program = chinachu.getProgramById(request.param.id, data.recorded);
-	
+
 	if (program === null) return response.error(404);
-	
+
 	if (!data.status.feature.filer) return response.error(403);
-	
-	if (!fs.existsSync(program.recorded)) return response.error(410);
-	
+
+	if (!fs.existsSync(program.recorded)){
+		response.setHeader('Cache-Control', 'no-cache');
+		response.setHeader('Pragma', 'no-cache');
+		return response.error(410);
+	}
+
+
+
 	switch (request.method) {
 		case 'GET':
 			var fstat = fs.statSync(program.recorded);
-			
+
 			if (request.type === 'm2ts') {
 				response.setHeader('content-length', fstat.size);
 				response.setHeader('content-disposition', 'attachment; filename="' + program.id + '.m2ts"');
@@ -19,10 +25,10 @@
 
 				fs.createReadStream(program.recorded).pipe(response);
 			}
-			
+
 			if (request.type === 'json') {
 				response.head(200);
-				
+
 				response.end(JSON.stringify({
 					dev    : fstat.dev,
 					ino    : fstat.ino,
@@ -39,14 +45,14 @@
 					ctime  : fstat.ctime.getTime()
 				}, null, '  '));
 			}
-			
+
 			return;
-		
+
 		case 'DELETE':
 			response.head(200);
-			
+
 			fs.unlinkSync(program.recorded);
-			
+
 			if (request.type === 'json') {
 				response.end('{}');
 			} else {
--- api/script-recorded-program.vm.js
+++ api/script-recorded-program.vm.js
@@ -34,6 +34,41 @@
 			response.head(200);
 			response.end('{}');
 			return;
+
+		case 'PUT':
+			if (!request.query.json) return response.error(400);
+
+			var obj = {};
+
+			try {
+				obj = JSON.parse(request.query.json);
+			} catch (e) {
+				return response.error(400);
+			}
+
+			data.recorded = (function() {
+				var array = [];
+
+				data.recorded.forEach(function(a) {
+					if (a.id !== program.id) {
+						array.push(a);
+					} else {
+						for (var i in obj) {
+							if ( i in a ){
+								a[i] = obj[i];
+							}
+						}
+						array.push(a);
+					}
+				});
+
+				return array;
+			})();
+			fs.writeFileSync(define.RECORDED_DATA_FILE, JSON.stringify(data.recorded));
+
+			response.head(200);
+			response.end('{}');
+			return;
 	}
 
 })();
